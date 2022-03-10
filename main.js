b4e7f0be0e0c1b8c40484e9d2c498cec7d53ad42
Shane Casiano <shane.casiano@gmail.com>
2022-03-10 01:45:47 -0500

    Make node work inside browser instance

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ global.readConfig();
 
 const createWindow = () => {
     const window = new BrowserWindow({
-      width: 800,
-      height: 600
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false
+        }
     });
     window.loadFile('html/index.html');
 }
