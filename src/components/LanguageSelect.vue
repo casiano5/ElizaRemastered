203e89b3aacb1779d1fd6f0ebbfdb0a45a6f9f5e
Shane Casiano <shane.casiano@gmail.com>
2022-03-22 20:09:32 -0400

    Added language selector

---
+++ src/components/LanguageSelect.vue
@@ -0,0 +1,9 @@
+<script setup>
+    const global = require('../js/globals');
+</script>
+
+<template>
+    <select>
+        <option :value="propertyName" v-for="(value, propertyName) in global.config.naturalSoundingLanguages" v-bind:key="propertyName">{{value}}</option>
+    </select>
+</template>
