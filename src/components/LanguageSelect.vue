8e98d3d717220792ef288e216a7c74290edff306
Shane Casiano <shane.casiano@gmail.com>
2022-03-22 21:34:37 -0400

    Added language selected is current lang

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -3,7 +3,25 @@
 </script>
 
 <template>
-    <select>
+    <select @change="changeLanguage($event)" v-model="selectedLanguage">
         <option :value="propertyName" v-for="(value, propertyName) in global.config.naturalSoundingLanguages" v-bind:key="propertyName">{{value}}</option>
     </select>
 </template>
+
+<script>
+    const global = require('../js/globals');
+    export default {
+        methods: {
+            changeLanguage(event){
+                global.config.language = event.target.value;
+                global.writeConfig();
+                console.log("Onupdate: " + this.selectedLanguage);
+            }
+        },
+        created(){
+            console.log(JSON.stringify(global.config));
+            this.selectedLanguage = global.config.language;
+        }
+    }
+</script>
+
