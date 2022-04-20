4f0cb7a8c7a04f66249c6c59a4aca36612464085
Tian <tianzhao51@gmail.com>
2022-04-20 15:44:07 -0700

    Change to toggle

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -2,34 +2,35 @@
 </script>
 
 <template>
-    <button type="button" class="btn btn-primary" @click="toggleDarkMode();">
-        <img :src = "imagePath">
-    </button>
+<div class="form-check form-switch" >
+<input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" @click="toggleDarkMode();">
+<label class="form-check-label" for="flexSwitchCheckDefault">Change Theme</label>
+</div>
 </template>
 
 <script>
-    const global = require('../js/globals')
-    export default { 
-        methods: {
-            toggleDarkMode(){
-                global.config.darkModeEnable = !global.config.darkModeEnable;
-                global.writeConfig();
-                if(this.imagePath === require("../assets/Lightbulb.svg")){
-                    this.imagePath = require("../assets/LightbulbFill.svg");
-                }
-                else {
-                    this.imagePath = require("../assets/Lightbulb.svg");
-                }
-                this.$emit("change-theme");
-            },
-        },
-        data () {
-            return {
-                imagePath: require("../assets/LightbulbFill.svg")
-            }
-        }
+const global = require('../js/globals')
+export default { 
+methods: {
+toggleDarkMode(){
+global.config.darkModeEnable = !global.config.darkModeEnable;
+global.writeConfig();
+if(this.imagePath === require("../assets/Lightbulb.svg")){
+this.imagePath = require("../assets/LightbulbFill.svg");
+}
+else {
+this.imagePath = require("../assets/Lightbulb.svg");
+}
+this.$emit("change-theme");
+},
+},
+data () {
+return {
+imagePath: require("../assets/LightbulbFill.svg")
+}
+}
 
-    }
+}
 </script>
 
 <style>
--- src/components/TextToSpeechButton.vue
+++ src/components/TextToSpeechButton.vue
@@ -2,32 +2,32 @@
 </script>
 
 <template>
-    <button type="button" class="btn btn-primary" @click="toggleSpeechToText">
-        <img :src = "imagePath">
-    </button>
+<div class="form-check form-switch" >
+<input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" @click="toggleSpeechToText">
+<label class="form-check-label" for="flexSwitchCheckDefault">TTS</label>
+</div>
 </template>
-
 
 <script>
 const global = require('../js/globals')
 export default {
-        methods: {
-            toggleSpeechToText(){
-                global.config.textToSpeechEnable = !global.config.textToSpeechEnable;
-                global.writeConfig();
-                if(this.imagePath === require("../assets/SpeakerOffIcon.svg")){
-                    this.imagePath = require("../assets/SpeakerIcon.svg");
-                }
-                else{
-                    this.imagePath = require("../assets/SpeakerOffIcon.svg");
-                }
-            }
-        },
-        data() {
-            return{
-                imagePath: require("../assets/SpeakerIcon.svg")
-            }
-        }
+methods: {
+toggleSpeechToText(){
+global.config.textToSpeechEnable = !global.config.textToSpeechEnable;
+global.writeConfig();
+if(this.imagePath === require("../assets/SpeakerOffIcon.svg")){
+this.imagePath = require("../assets/SpeakerIcon.svg");
+}
+else{
+this.imagePath = require("../assets/SpeakerOffIcon.svg");
+}
+}
+},
+data() {
+return{
+imagePath: require("../assets/SpeakerIcon.svg")
+}
+}
 }
 </script>
 <style>
